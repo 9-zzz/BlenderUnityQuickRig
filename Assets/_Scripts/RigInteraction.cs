e947d5ff3649e8a17d1b0cd3b2d54b3dde227beb
Shahan Akhter <shahan3.14@gmail.com>
2019-07-18 11:55:31 -0400

    added rig interaction script

---
+++ Assets/_Scripts/RigInteraction.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigInteraction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            print("hit");
+        }
+    }
+
+}
--- Assets/_Scripts/SimplePlayer.cs
+++ Assets/_Scripts/SimplePlayer.cs
@@ -1,26 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-// This is an extremely quick and dirty demo-only 2D character controller.
-// The focus of this tutorial is on the other mechanics.
-// I put physics-2D-material with zero friction on the collider to stop from "sticking" to walls.
 public class SimplePlayer: MonoBehaviour
 {
-    //public static Player2DMovement S;
+    Rigidbody rb;
 
-    // Set mass to 100. Set gravity scale to 3. Freeze rotation along Z-axis.
-    Rigidbody rb2d;
+    public int health = 100;
 
     public float speed = 5.0f;
     public float jumpSpeed = 12.0f;
 
-    public bool canJump = false;
-    public bool canMove = true;
-
     void Awake()
     {
         //S = this;
-        rb2d = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Explains 'FixedUpdate' -> http://unity3d.com/learn/tutorials/topics/scripting/update-and-fixedupdate
@@ -29,24 +22,16 @@ public class SimplePlayer: MonoBehaviour
         // Explains 'GetAxis' -> https://unity3d.com/learn/tutorials/topics/scripting/getaxis
         float xinput = Input.GetAxis("Horizontal");
 
-        if (canMove)
-            rb2d.velocity = new Vector2(xinput * speed, rb2d.velocity.y);
+        rb.velocity = new Vector2(xinput * speed, rb.velocity.y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow) && canJump)
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpSpeed);
-            //canJump = false;
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
         }
-    }
-
-    void OnCollisionEnter(Collision other)
-    {
-        //if (other.gameObject.tag == "Ground")
-            //canJump = true;
     }
 
 }
